c008dc991275a6be79100bf846f07605be9112a1
vint78912131 <starozhil2008@mail.ru>
2023-03-10 21:40:53 +0300

    task2205

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    /*
+    МНЕ нравится курс JavaRush
+    */
+    public static void main(String[] args) {
+        System.out.println(String.format(getFormattedString(), "JavaRush", "курс", "мне", "нравится"));
+        //должен быть вывод
+        //"МНЕ нравится курс JavaRush"
+    }
+
+    public static String getFormattedString() {
+       return  ("%3$s").toUpperCase()+" %4$s %2$s %1$s";
+    }
+}
